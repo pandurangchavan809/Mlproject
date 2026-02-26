19b18aa7e4cf6383c07c9fcd4b069c4804a498d9
pandurangchavan809 <pandurangchavan809@gmail.com>
2026-02-26 23:53:40 +0530

    data_ingestion done

---
+++ src/components/data_transformation.py
@@ -0,0 +1,20 @@
+# feature enginerring, transform the data etc.., do here
+import os
+import sys
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
+
+class class 
