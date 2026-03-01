7da4bef0d294915dbfead117d94b306efede0064
pandurangchavan809 <pandurangchavan809@gmail.com>
2026-03-01 11:32:04 +0530

    Removed catboost training logs

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -28,14 +28,14 @@ class DataTransformation:
 
     def get_data_transformer_object(self):
         try:
-            numerical_columns = ["writing_score", "reading_score"]
+            numerical_columns = ["writing score", "reading score"]
 
             categorical_columns = [
                 "gender",
-                "race_ethnicity",
-                "parental_level_of_education",
+                "race/ethnicity",
+                "parental level of education",
                 "lunch",
-                "test_preparation_course",
+                "test preparation course",
             ]
 
             num_pipeline = Pipeline(
@@ -77,7 +77,7 @@ class DataTransformation:
 
             preprocessing_obj = self.get_data_transformer_object()
 
-            target_column_name = "math_score"
+            target_column_name = "math score"
 
             input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
             target_feature_train_df = train_df[target_column_name].values
@@ -109,13 +109,11 @@ class DataTransformation:
         except Exception as e:
             raise CustomException(e, sys)
 
-#
-
 if __name__ == "__main__":
     from src.components.data_ingestion import DataIngestion
 
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
-
+ 
     data_transformation = DataTransformation()
     data_transformation.initiate_data_transformation(train_data, test_data)
