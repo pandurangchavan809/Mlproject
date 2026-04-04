17bd4e27b0ec434eecdc3743d97d13e0a1894be8
pandurangchavan809 <pandurangchavan809@gmail.com>
2026-04-04 23:59:17 +0530

    app.py streamlit app for lstm project

--- Next_word_prediction_LSTM/app.py
+++ Next_word_prediction_LSTM/app.py
@@ -24,7 +24,7 @@ def predict_next_word(model, tokenizer, text, max_sequence_len):
             return word
     return None
 
-# streamlit app LST
+# streamlit app LSTM
 st.title("Next Word Prediction With LSTM And Early Stopping")
 input_text=st.text_input("Enter the sequence of Words","To be or not to")
 if st.button("Predict Next Word"):
